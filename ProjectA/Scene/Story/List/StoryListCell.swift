9c4338cbedcb4aad42c942bc6ded39bfc353cdb8
cg-yeong <ds1506027@gmail.com>
2021-11-25 10:51:31 +0900

    사연 리스트 파일 추가

---
+++ ProjectA/Scene/Story/List/StoryListCell.swift
@@ -0,0 +1,20 @@
+//
+//  StoryListCell.swift
+//  ProjectA
+//
+//  Created by inforex on 2021/11/25.
+//
+
+import UIKit
+
+class StoryListCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnail_imageView: UIImageView!
+    @IBOutlet weak var gender: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var sentDate: UILabel!
+    @IBOutlet weak var conts_view: UIView!
+    @IBOutlet weak var conts_label: UILabel!
+    @IBOutlet weak var more_button: UIButton!
+    
+}
---
+++ ProjectA/Scene/Story/List/StoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  StoryListView.swift
+//  ProjectA
+//
+//  Created by inforex on 2021/11/25.
+//
+
+import UIKit
+import SwiftyJSON
+import RxSwift
+import RxCocoa
+
+class StoryListView: XibView {
+    
+    @IBOutlet weak var another_view: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var close_button: UIButton!
+    @IBOutlet weak var storyNone_view: UIView!
+    
+}
